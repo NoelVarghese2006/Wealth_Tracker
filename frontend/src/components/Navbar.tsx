5ade6e59cb8758700a111d401732c9300cd558ce
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-06-09 12:48:50 -0500

    navbar-1

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import { TbSunMoon } from 'react-icons/tb'
+import { FaRegUser } from "react-icons/fa";
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex flex-row items-center justify-between w-full p-4'>
+        <div>Wealth Tracker</div>
+        <div className='flex flex-row items-center justify-between gap-4 p-4'>
+            <FaRegUser className='text-2xl' />
+            <TbSunMoon className='text-2xl'/>
+        </div>
+    </div>
   )
 }
 
