38449b3fb9925ea7ed14a1d24928b7f9acbe45ad
Noel Varghese <91435868+NoelVarghese2006@users.noreply.github.com>
2025-06-10 23:22:10 -0500

    initial-navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,22 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
 import Navbar from './components/Navbar'
+import LoginPage from './pages/LoginPage'
+import { ThemeProvider } from './components/ThemeProvider'
 
 function App() {
 
   return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="top-0 right-0 p-0 m-0 flex flex-col items-start min-h-screen">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
           <Route path='/signup' element={<SignUpPage />} />
         </Routes>
       </div>
+    </ThemeProvider>
   )
 }
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import { TbSunMoon } from 'react-icons/tb'
 import { FaRegUser } from "react-icons/fa";
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import { useTheme } from '@/components/ThemeProvider';
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
     <div className='flex flex-row items-center justify-between w-full p-4'>
-        <div>Wealth Tracker</div>
-        <div className='flex flex-row items-center justify-between gap-4 p-4'>
-            <FaRegUser className='text-2xl' />
-            <TbSunMoon className='text-2xl'/>
+        <div className='font-extrabold text-2xl uppercase'>
+          <Link to={"/"}>Wealth Tracker</Link>
+        </div>
+        <div className='flex flex-row items-center justify-between gap-1 p-4'>
+          <Link to={"/login"}>
+            <Button><FaRegUser className='text-2xl' /></Button>
+          </Link>
+            <Button onClick={toggleTheme}><TbSunMoon className='text-2xl'/></Button>
         </div>
     </div>
   )
